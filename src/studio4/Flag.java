f8f3940a44684e5be27d3f80c2d9ea631346617b
Yael <112411257+YaelM13@users.noreply.github.com>
2024-02-22 11:04:41 -0600

    circle flag !

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(217,233,196);
+		StdDraw.filledCircle(.5, .5, .5);
+		
+		StdDraw.setPenColor(196,233,230);
+		StdDraw.filledCircle(.5, .5, .4);
+		
+		StdDraw.setPenColor(196,213,233);
+		StdDraw.filledCircle(.5, .5, .3);
+		
+		StdDraw.setPenColor(196,197,233);
+		StdDraw.filledCircle(.5, .5, .2);
+		
+		StdDraw.setPenColor(228,196,233);
+		StdDraw.filledCircle(.5, .5, .1);
+		
 	}
 }
